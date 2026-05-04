bca160a7a3997f4842f63cab1ae7197422c357dd
npp-zep <264519049@qq.com>
2026-05-04 18:27:10 +0800

    Remove leftover test_proxy_print.lua

--- test_proxy_print.lua
+++
@@ -1,5 +0,0 @@
-local Runnable = java.import('java.lang.Runnable')
-local handler = { run = function(self) end }
-local proxy = java.createProxy({'java.lang.Runnable'}, handler)
-local s = tostring(proxy)
-print('type=' .. type(proxy) .. ', tostring=' .. s)
