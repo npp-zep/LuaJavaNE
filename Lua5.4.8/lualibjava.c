5deac924ecd407309f13bc565530d720c9597de2
npp-zep <264519049@qq.com>
2026-05-08 05:27:15 +0800

    Fix store: reuse existing key, prevent crash on many keys

--- Lua5.4.8/lualibjava.c
+++ Lua5.4.8/lualibjava.c
@@ -921,25 +921,36 @@ static StoreEntry* store_registry = NULL;
 static int java_store(lua_State* L) {
     const char* key = luaL_checkstring(L, 1);
     StoreEntry* e = store_registry;
-    while (e) { if (strcmp(e->key, key) == 0) break; e = e->next; }
-    if (!e) {
-        e = (StoreEntry*)malloc(sizeof(StoreEntry));
-        e->key = strdup(key);
-        e->next = store_registry;
-        store_registry = e;
+    while (e) {
+        if (strcmp(e->key, key) == 0) {
+            if (e->strVal) { free(e->strVal); e->strVal = NULL; }
+            int t = lua_type(L, 2);
+            e->type = t;
+            switch (t) {
+                case LUA_TNUMBER: e->numVal = lua_tonumber(L, 2); break;
+                case LUA_TSTRING: e->strVal = strdup(lua_tostring(L, 2)); break;
+                case LUA_TBOOLEAN: e->boolVal = lua_toboolean(L, 2); break;
+                default: e->type = LUA_TNIL; break;
+            }
+            return 0;
+        }
+        e = e->next;
     }
-    if (e->strVal) { free(e->strVal); e->strVal = NULL; }
+    e = (StoreEntry*)malloc(sizeof(StoreEntry));
+    e->key = strdup(key);
+    e->strVal = NULL;
     int t = lua_type(L, 2);
     e->type = t;
     switch (t) {
         case LUA_TNUMBER: e->numVal = lua_tonumber(L, 2); break;
         case LUA_TSTRING: e->strVal = strdup(lua_tostring(L, 2)); break;
         case LUA_TBOOLEAN: e->boolVal = lua_toboolean(L, 2); break;
-        default: break;
+        default: e->type = LUA_TNIL; break;
     }
+    e->next = store_registry;
+    store_registry = e;
     return 0;
 }
-
 static int java_fetch(lua_State* L) {
     const char* key = luaL_checkstring(L, 1);
     StoreEntry* e = store_registry;
