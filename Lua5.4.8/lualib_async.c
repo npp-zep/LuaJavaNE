3ce169e86206fa1c974ade267ec97aea239a6671
npp-zep <264519049@qq.com>
2026-05-09 23:20:07 +0800

    Complete async runAsync with type support and lock

--- Lua5.4.8/lualib_async.c
+++ Lua5.4.8/lualib_async.c
@@ -9,6 +9,7 @@
 
 extern JavaVM* g_jvm;
 extern pthread_mutex_t lua_mutex;
+static pthread_mutex_t async_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct PromiseEntry {
     int id;
@@ -32,22 +33,37 @@ static void* run_async_thread(void* arg) {
 
     JNIEnv* env = NULL;
     (*g_jvm)->AttachCurrentThread(g_jvm, &env, NULL);
+    if (env == NULL) { fprintf(stderr, "AttachCurrentThread failed\n"); return NULL; }
 
     lua_settop(mainL, 0);
     lua_rawgeti(mainL, LUA_REGISTRYINDEX, task->funcRef);
     int err = lua_pcall(mainL, 0, 1, 0);
+    if (err == LUA_OK) fprintf(stderr, "pcall ok\n"); else fprintf(stderr, "pcall err=%d: %s\n", err, lua_tostring(mainL, -1));
 
     if (err == LUA_OK) {
-        const char* val = lua_tostring(mainL, -1);
+        char resultStr[256] = "";
+        if (lua_isstring(mainL, -1)) {
+            snprintf(resultStr, sizeof(resultStr), "S:%s", lua_tostring(mainL, -1));
+        } else if (lua_isinteger(mainL, -1)) {
+            snprintf(resultStr, sizeof(resultStr), "I:%lld", (long long)lua_tointeger(mainL, -1));
+        } else if (lua_isnumber(mainL, -1)) {
+            snprintf(resultStr, sizeof(resultStr), "N:%.15g", lua_tonumber(mainL, -1));
+        } else if (lua_isboolean(mainL, -1)) {
+            snprintf(resultStr, sizeof(resultStr), "B:%d", lua_toboolean(mainL, -1));
+        } else {
+            snprintf(resultStr, sizeof(resultStr), "X:");
+        }
         lua_pop(mainL, 1);
 
+        pthread_mutex_lock(&async_mutex);
         PromiseEntry* entry = promise_registry;
         while (entry) { if (entry->id == task->promiseId) break; entry = entry->next; }
         if (entry) {
             if (entry->result) free(entry->result);
-            entry->result = val ? strdup(val) : strdup("");
+            entry->result = strdup(resultStr);
             entry->done = 1;
         }
+        pthread_mutex_unlock(&async_mutex);
     }
 
     luaL_unref(mainL, LUA_REGISTRYINDEX, task->funcRef);
@@ -76,16 +92,29 @@ int java_runAsync(lua_State* L) {
 
 int java_checkPromise(lua_State* L) {
     int id = (int)luaL_checkinteger(L, 1);
+    pthread_mutex_lock(&async_mutex);
     PromiseEntry* entry = promise_registry;
     while (entry) {
         if (entry->id == id) {
             lua_pushboolean(L, entry->done);
-            if (entry->done && entry->result) lua_pushstring(L, entry->result);
-            else lua_pushnil(L);
+            if (entry->done && entry->result) {
+                char* r = entry->result;
+                switch (r[0]) {
+                    case 'S': lua_pushstring(L, r + 2); break;
+                    case 'I': lua_pushinteger(L, atoll(r + 2)); break;
+                    case 'N': lua_pushnumber(L, atof(r + 2)); break;
+                    case 'B': lua_pushboolean(L, r[2] == '1'); break;
+                    default: lua_pushnil(L); break;
+                }
+            } else {
+                lua_pushnil(L);
+            }
+            pthread_mutex_unlock(&async_mutex);
             return 2;
         }
         entry = entry->next;
     }
+    pthread_mutex_unlock(&async_mutex);
     lua_pushboolean(L, 0);
     lua_pushnil(L);
     return 2;
