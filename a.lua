433319422b3681e778615d02bc5b53f7c66ad1b5
npp-zep <264519049@qq.com>
2026-05-16 21:10:42 +0800

    Remove leftover benchmark and test scripts from agent-v2 development

--- a.lua
+++
@@ -1,7 +0,0 @@
--- 用 Lua 写 Java 程序，不需要安装 JDK
-local System = java.import("java.lang.System")
-local Files = java.import("java.nio.file.Files")
-local Paths = java.import("java.nio.file.Paths")
-
-Files:writeString(Paths:get("test.txt"), "Hello from Lua")
-print("Java version:", System:getProperty("java.version"))
--- test_err_id.lua
+++
@@ -1,5 +0,0 @@
-java = require("java")
-
-print("id1:", java.promise())
-print("id2:", java.promise())
-print("id3:", java.promise())
